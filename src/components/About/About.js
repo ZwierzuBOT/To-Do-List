8d8fce384b29f7d9bfabb0779d3b7d988888b569
Mateusz Zwierzchowski <125812607+ZwierzuBOT@users.noreply.github.com>
2023-12-10 22:05:17 +0100

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,1 +1,15 @@
 
+
+import "./About.css"
+
+
+
+const About = () => {
+    return ( 
+        <div className="AboutDiv">
+            <h1>About</h1>
+        </div>
+     );
+}
+ 
+export default About;
