d0112cf9cac9f37adca8bc7b1051669c272caa08
Mateusz Zwierzchowski <125812607+ZwierzuBOT@users.noreply.github.com>
2023-12-13 23:00:46 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,80 @@
 
 import "./Contact.css"
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
+
+
+
 
 
 
 const Contact = () => {
+
+    let [BoxVis, setBoxVis] = useState("HideBox");
+/*
+    function BoxFunc(idk){
+        if (idk === "CloseOpen"){
+            BoxVis === "HideBox" ? setBoxVis("SuccessBox") : setBoxVis("HideBox");
+        }
+        else{
+            setBoxVis("SuccessBox");
+        }
+
+    }
+
+*/
+    function Hide(){
+        setBoxVis("HideBox");
+    }
+
+    function OnlyOpen(){
+        setBoxVis("SuccessBox");
+    }
+
+
+    const form = useRef();
+
+
+    const sendEmail = (e) =>{
+        e.preventDefault();
+
+        emailjs
+        .sendForm(
+            "service_9y3koq7",
+            "template_k7uh9np",
+            form.current,
+            "W59nLwNdrbBpt3yJl"
+        )
+        .then(
+            (result) => {
+
+            }
+        )
+    };
+
+
+
     return ( 
         <div className="ContactDiv">
-            <h1>Contact</h1>
+            <form ref={form} onSubmit={sendEmail}>
+                <h1>Contact Me</h1>
+                <h5>zwierzchowski.mateo@gmail.com</h5>
+
+                <h3>Your Name</h3>
+                <input type="text" name="user_name"/>
+                <h3>Your Email</h3>
+                <input type="email" name="user_email"/>
+                <h3>Message</h3>
+                <textarea id="textArea"  name="message"></textarea>
+                <button type="submit" value="Send" onClick={OnlyOpen}>Send</button>
+            </form>
+
+
+
+            <div className={BoxVis}>
+                <button className="Close" onClick={Hide}>X</button>
+                <h4>Your message has been sent successfully!</h4>
+            </div>
         </div>
      );
 }
